b47e78ffee13325b9ec5876be7a2aa4fad8d749a
ltmx <nicolasreinhard.pro@gmail.com>
2024-10-15 12:52:26 +0200

    Added xml Documentation to UIElements.Extensions.Create and UIElements.Extensions.Edition

--- Runtime/UIElements.Extensions.Create.cs
+++ Runtime/UIElements.Extensions.Create.cs
@@ -12,54 +12,96 @@ using UnityEngine.UIElements;
 
 public static partial class UIToolKitExtensions
 {
+    /// <summary>Creates a new VisualElement</summary>
     public static VisualElement VisualElement() => new();
+
+    /// <summary>Creates a new TextElement</summary>
     public static TextElement TextElement() => new();
+
+    /// <summary>Creates a new TextField</summary>
     public static TextField TextField() => new();
+
+    /// <summary>Creates a new Button</summary>
     public static Button Button() => new();
+
+    /// <summary>Creates a new Label</summary>
     public static Label Label() => new();
+
+    /// <summary>Creates a new Image</summary>
     public static Image Image() => new();
 
+    /// <summary>Creates a new VisualElement of type T while parenting it to a container Element</summary>
     public static T Make<T>(this VisualElement container) where T : VisualElement, new() => new T().Container(container);
 
+    /// <summary>Creates and returns new VisualElement while parenting it to a container Element</summary>
     public static VisualElement VisualElement(this VisualElement container) => new VisualElement().Container(container);
+
+    /// <summary>Creates and returns a new DropDownField while parenting it to a container Element</summary>
     public static DropdownField DropdownField(this VisualElement container) => new DropdownField().Container(container);
+
+    /// <summary>Creates and returns a new TextElement while parenting it to a container Element</summary>
     public static TextElement TextElement(this VisualElement container) => new TextElement().Container(container);
+
+    /// <summary>Creates and returns a new TextField while parenting it to a container Element</summary>
     public static TextField TextField(this VisualElement container) => new TextField().Container(container);
+
+    /// <summary>Creates and returns a new Button while parenting it to a container Element</summary>
     public static Button Button(this VisualElement container) => new Button().Container(container);
+
+    /// <summary>Creates and returns a new Label while parenting it to a container Element</summary>
     public static Label Label(this VisualElement container) => new Label().Container(container);
+
+    /// <summary>Creates and returns a new Image while parenting it to a container Element</summary>
     public static Image Image(this VisualElement container) => new Image().Container(container);
-    
+
+    /// <summary>Creates and returns a new Slider while parenting it to a container Element</summary>
     public static Slider Slider(this VisualElement container) => new Slider().Container(container);
+
+    /// <inheritdoc cref="Slider(UnityEngine.UIElements.VisualElement)" />
     public static Slider Slider(this VisualElement container, float min, float max) => new Slider(min, max).Container(container);
-    public static Slider Slider(this VisualElement container, float min, float max, SliderDirection dir,  float value) => new Slider(min, max, dir, value).Container(container);
 
+    /// <inheritdoc cref="Slider(UnityEngine.UIElements.VisualElement)" />
+    public static Slider Slider(this VisualElement container, float min, float max, SliderDirection dir, float value) => new Slider(min, max, dir, value).Container(container);
 
+    /// <summary>Creates and returns a new Image while parenting it to a container Element</summary>
     public static Image Image(this VisualElement container, Texture2D texture) => new Image { image = texture }.Container(container);
 
+    /// <summary>Creates and returns a new Label while parenting it to a container Element</summary>
     public static Label Label(this VisualElement container, string text) => new Label(text).Container(container);
 
+    /// <summary>Creates and returns a new Button while parenting it to a container Element</summary>
     public static Button Button(this VisualElement container, Action clickEvent) => new Button(clickEvent).Container(container);
 
+    /// <summary>Sets the onClickEvent called when clicking a button and return</summary>
+    /// <returns>button</returns>
     public static Button Event(this Button button, Action clickEvent)
     {
         button.clickable = new Clickable(clickEvent);
         return button;
     }
 
+    /// <summary>Creates and returns a new Button with given parameters</summary>
     public static Button Button(string text, Action clickEvent) => Button().Text(text).Event(clickEvent);
+
+    /// <summary>Creates and returns a new Button with given parameters</summary>
     public static Button Button(Action clickEvent, string text) => Button().Text(text).Event(clickEvent);
 
+    /// <summary>Adds an action to the button's onClickEvent</summary>
+    /// <returns>the button</returns>
     public static Button AddEvent(this Button button, Action clickEvent)
     {
         button.clickable.clicked += clickEvent;
         return button;
     }
 
+    /// <summary>Cleans the TextField's input field</summary>
+    /// <returns>the TextField</returns>
     public static void Clean(this TextField textField) => textField.value = textField.value.Clean();
 
-    public static T With<T, U>(this T v, U child) where T : VisualElement where U : VisualElement
+    /// <summary>Parents a child to element and returns element</summary>
+    public static T With<T, U>(this T element, U child) where T : VisualElement where U : VisualElement
     {
-        v.Add(child);
-        return v;
+        element.Add(child);
+        return element;
     }
 }
--- Runtime/UIElements.Extensions.Edition.cs
+++ Runtime/UIElements.Extensions.Edition.cs
@@ -11,21 +11,30 @@ using UnityEngine.UIElements;
 
 public static partial class UIToolKitExtensions
 {
+    /// <summary>
+    /// Adds USS Styling to a VisualElement
+    /// <li>ShortCut for VisualElement.AddToClassList(...)</li>
+    /// </summary>
+    /// <returns>the VisualElement</returns>
+    /// <seealso cref="VisualElement.AddToClassList"/>
     public static T USS<T>(this T e, string A) where T : VisualElement {
         e.AddToClassList(A);
         return e;
     }
+    /// <inheritdoc cref="USS{T}(T,string)"/>
     public static T USS<T>(this T e, string A, string B) where T : VisualElement {
         e.AddToClassList(A);
         e.AddToClassList(B);
         return e;
     }
+    /// <inheritdoc cref="USS{T}(T,string)"/>
     public static T USS<T>(this T e, string A, string B, string C) where T : VisualElement {
         e.AddToClassList(A);
         e.AddToClassList(B);
         e.AddToClassList(C);
         return e;
     }
+    /// <inheritdoc cref="USS{T}(T,string)"/>
     public static T USS<T>(this T e, string A, string B, string C, string D) where T : VisualElement {
         e.AddToClassList(A);
         e.AddToClassList(B);
@@ -34,56 +43,90 @@ public static partial class UIToolKitExtensions
         return e;
     }
 
+    /// <summary>
+    /// Returns a list of elements having a specific USS class
+    /// </summary>
     public static IEnumerable<VisualElement> WhereClassListContains(this IEnumerable<VisualElement> e, string A)
     {
         return e.Where(x => x.ClassListContains(A));
     }
+    
+    /// <summary>
+    /// Returns a list of elements not having a specific USS class
+    /// </summary>
     public static IEnumerable<VisualElement> WhereClassListMisses(this IEnumerable<VisualElement> e, string A)
     {
         return e.Where(x => x.ClassListContains(A) == false);
     }
-    
+    /// <summary>
+    /// Returns a list of child elements having a specific USS class
+    /// </summary>
     public static IEnumerable<VisualElement> ChildrenWithClass(this VisualElement e, string A)
     {
         return e.Children().Where(x => x.ClassListContains(A));
     }
+    /// <summary>
+    /// Returns the first child element having a specific USS class
+    /// </summary>
     public static VisualElement FirstChildWithClass(this VisualElement e, string A)
     {
         return e.Children().FirstOrDefault(x => x.ClassListContains(A));
     }
+    
+    /// <summary>
+    /// Returns the last child element having a specific USS class
+    /// </summary>
     public static VisualElement LastChildWithClass(this VisualElement e, string A)
     {
         return e.Children().LastOrDefault(x => x.ClassListContains(A));
     }
     
+    /// <summary>
+    /// Adds USS Styling to every element of a IEnumerable{VisualElement}
+    /// </summary>
+    /// <returns>the VisualElement</returns>
+    /// <seealso cref="VisualElement.AddToClassList"/>
     public static IEnumerable<T> USS<T>(this IEnumerable<T> e, string A) where T : VisualElement {
         return e.ForEachAndReturn(x => x.USS(A));
     }
+    
+    /// <inheritdoc cref="USS{T}(IEnumerable{T},string)"/>
     public static IEnumerable<T> USS<T>(this IEnumerable<T> e, string A, string B) where T : VisualElement {
         return e.ForEachAndReturn(x => x.USS(A, B));
     }
+    /// <inheritdoc cref="USS{T}(IEnumerable{T},string)"/>
     public static IEnumerable<T> USS<T>(this IEnumerable<T> e, string A, string B, string C) where T : VisualElement {
         return e.ForEachAndReturn(x => x.USS(A, B, C));
     }
+    /// <inheritdoc cref="USS{T}(IEnumerable{T},string)"/>
     public static IEnumerable<T> USS<T>(this IEnumerable<T> e, string A, string B, string C, string D) where T : VisualElement {
         return e.ForEachAndReturn(x => x.USS(A, B, C, D));
     }
 
+    /// <summary>
+    /// Removes USS Styling to a VisualElement
+    /// <li>ShortCut for VisualElement.RemoveFromClassList(...)</li>
+    /// </summary>
+    /// <returns>the VisualElement</returns>
+    /// <seealso cref="VisualElement.RemoveFromClassList"/>
     public static T RemoveUSS<T>(this T e, string A) where T : VisualElement {
         e.RemoveFromClassList(A);
         return e;
     }
+    /// <inheritdoc cref="RemoveUSS{T}(T,string)"/>
     public static T RemoveUSS<T>(this T e, string A, string B) where T : VisualElement {
         e.RemoveFromClassList(A);
         e.RemoveFromClassList(B);
         return e;
     }
+    /// <inheritdoc cref="RemoveUSS{T}(T,string)"/>
     public static T RemoveUSS<T>(this T e, string A, string B, string C) where T : VisualElement {
         e.RemoveFromClassList(A);
         e.RemoveFromClassList(B);
         e.RemoveFromClassList(C);
         return e;
     }
+    /// <inheritdoc cref="RemoveUSS{T}(T,string)"/>
     public static T RemoveUSS<T>(this T e, string A, string B, string C, string D) where T : VisualElement {
         e.RemoveFromClassList(A);
         e.RemoveFromClassList(B);
@@ -98,48 +141,64 @@ public static partial class UIToolKitExtensions
         return e;
     }
     
-    // about 8 times faster than the Linq version using the Query() method as a selector
+    // 
+    /// <summary>
+    /// Removes USS Styling to every element of a IEnumerable{VisualElement}
+    /// <li>about 8 times faster than the Linq version using the Query() method as a selector</li>
+    /// </summary>
+    /// <returns>the VisualElement</returns>
+    /// <seealso cref="VisualElement.RemoveFromClassList"/>
     public static IEnumerable<T> RemoveUSS<T>(this IEnumerable<T> e, string A) where T : VisualElement
     {
         return e.ForEachAndReturn(x => x.RemoveFromClassList(A));
     }
     
+    /// <summary>
+    /// Removes all USS Styling from a given VisualElement
+    /// </summary>
     public static T ClearUSS<T>(this T e) where T : VisualElement {
         e.ClearClassList();
         return e;
     }
     
+    /// <summary>
+    /// Removes all USS Styling from a given VisualElement and then adds a new styling
+    /// </summary>
     public static T OverrideUSS<T>(this T e, string A) where T : VisualElement
     {
         return e.ClearUSS().USS(A);
     }
 
-    public static T Container<T, U>(this T element, U container) where T : VisualElement where U : VisualElement {
+    /// <summary>Sets the parent container of a Given VisualElement</summary>
+    /// <returns>The now parented child VisualElement</returns>
+    public static T Container<T, U>(this T element, U container) where T : VisualElement where U : VisualElement
+    {
         container.Add(element);
         return element;
     }
     
-    public static VisualElement Insert(this VisualElement v, VisualElement v2)
-    {
-        v.Add(v2);
-        return v;
-    }
-    
-    public static void InsertBefore(this VisualElement v, VisualElement v2)
+    /// <summary>
+    /// Inserts a VisualElement Before the given element in hierarchy
+    /// </summary>
+    public static void InsertBefore<T>(this VisualElement v, T v2) where T : VisualElement
     {
         if (v.parent == null) {
             throw new NullReferenceException(v.name + " has no parent");
         }
         v.parent.Insert(v.parent.IndexOf(v), v2);
     }
-    public static void InsertAfter(this VisualElement v, VisualElement v2)
+    /// <summary>
+    /// Inserts a VisualElement After the given element in hierarchy
+    /// </summary>
+    public static void InsertAfter<T>(this VisualElement v, T v2) where T : VisualElement
     {
         if (v.parent == null) {
             throw new NullReferenceException(v.name + " has no parent");
         }
         v.parent.Insert(v.parent.IndexOf(v) +1, v2);
     }
-    
+
+    /// <summary>Adds a list of VisualElements as children elements to a given VisualElement</summary>
     public static void AddMany<T>(this VisualElement v, T elements) where T : IEnumerable<VisualElement>
     {
         foreach (var e in elements)
